fcdcf755e8a7afedafd6d09579b8d53067b46f71
Zelalem <zzewdie@gmail.com>
2022-11-29 23:16:35 +0300

    PyProg_ifelse_task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,23 @@
 import random
 number = random.randint(-10000, 10000)
 if abs(number) % 10 > 5:
-    print("Last digit of %d is %d and is greater than 5" % (number, (abs(number) % 10)))
+    if number < 0:
+        print("Last digit of %d is %d and is greater than 5"
+                % (number, ((abs(number) % 10) * -1)))
+    else:
+        print("Last digit of %d is %d and is greater than 5"
+            % (number, (abs(number) % 10)))
 elif abs(number) % 10 == 0:
-    print("Last digit of %d is %d and is 0" % (number, (abs(number) % 10)))
+    if number < 0:
+        print("Last digit of %d is %d and is 0"
+                % (number, ((abs(number) % 10) * -1)))
+    else:
+        print("Last digit of %d is %d and is 0"
+                % (number, (abs(number) % 10)))
 if (abs(number) % 10 < 6) and (abs(number) % 10 != 0):
-    print("Last digit of %d is %d and is less than 6 and not 0" % (number, (abs(number) % 10)))
+    if number < 0:
+        print("Last digit of %d is %d and is less than 6 and not 0"
+                % (number, ((abs(number) % 10) * -1)))
+    else:
+        print("Last digit of %d is %d and is less than 6 and not 0"
+                % (number, (abs(number) % 10)))
