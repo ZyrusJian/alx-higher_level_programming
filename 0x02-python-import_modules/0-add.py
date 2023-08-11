d521dc702225808b20caf03105a494fbaddc79c7
Zelalem <zzewdie@gmail.com>
2023-08-11 21:28:09 +0300

    0x02_py_import_v0b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add(a, b)
+    result = add(a, b)
 
-print("{0} + {1} = {2}".format(a, b, result))
+    print("{0} + {1} = {2}".format(a, b, result))
