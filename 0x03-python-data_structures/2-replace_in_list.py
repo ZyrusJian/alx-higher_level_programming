98b82e6bf856ed186340c66da4f574c664261d20
Zelalem <zzewdie@gmail.com>
2023-08-14 17:16:44 +0300

    0x03_py_datastruct_v2b

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx, new_element):
+def replace_in_list(my_list, idx, new_element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = new_element
