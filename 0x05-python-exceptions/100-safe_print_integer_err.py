bb3fc2eb2bf1100ebb8d18a9f60f20a5a9220600
Zelalem <zzewdie@gmail.com>
2023-08-28 23:21:08 +0300

    0x05_py_exceptions_100b

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        integer_value = int(value)
-        print("{:d}".format(integer_value))
+        print("{:d}".format(value))
         return True
     except ValueError:
         print("Exception: Unknown format code 'd' for
