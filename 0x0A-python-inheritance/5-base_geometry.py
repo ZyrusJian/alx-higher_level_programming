5157f99d8720f12451e8638e91eb9910a9da47b9
Zelalem <zzewdie@gmail.com>
2023-10-01 21:42:29 +0300

    py_inheritance_5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class that serves as a base for other geometry classes.
+    """
+    pass
