fc707bdb5772e78e61e11648f60056a7a1c458d2
Zelalem <zzewdie@gmail.com>
2023-10-02 16:25:35 +0300

    py_InputOutput_7c

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
 """
-This script adds all arguments to a Python list, and then saves them as
-a JSON representation in a file named (add_item.json).
+This script adds all arguments to a Python list, and then saves them to a file.
 """
 
 import sys
 from os import path
-from typing import List
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = 'add_item.json'
+my_list = []
 
-def add_item(args: List[str], filename: str) -> List[str]:
-    """
-    Adds all arguments to a Python list, and then saves them to a file.
+if path.exists(filename):
+    my_list = load_from_json_file(filename)
 
-    Args:
-        args (List[str]): The list of arguments to add to the list.
-        filename (str): The name of the file to save the list to.
+my_list.extend(sys.argv[1:])
 
-    Returns:
-        List[str]: The updated list of items.
-    """
-    if path.exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-
-    my_list.extend(args)
-    save_to_json_file(my_list, filename)
-    return my_list
-
-
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    add_item(args, 'add_item.json')
+save_to_json_file(my_list, filename)
