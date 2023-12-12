cb30708b8109204fd144f9bc3682cf26ca174a7e
Zelalem <zzewdie@gmail.com>
2023-12-12 22:50:43 +0300

    0x0D_sql_intro_1_v2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,14 +1,3 @@
 -- This script creates the database hbtn_0c_0 if it doesn't already exist
 -- Create the database if it doesn't exist
-SET @create_query = IF(
-	EXISTS(
-		SELECT SCHEMA_NAME
-		FROM INFORMATION_SCHEMA.SCHEMATA
-		WHERE SCHEMA_NAME = 'hbtn_0c_0'
-	),
-	'',
-	'CREATE DATABASE hbtn_0c_0'
-);
-PREPARE create_stmt FROM @create_query;
-EXECUTE create_stmt;
-DEALLOCATE PREPARE create_stmt;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
